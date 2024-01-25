4c5cd7ae3487655bd67da474600670d2b0c353f5
mirazfaruk <mirazfaruk@gmail.com>
2024-01-25 23:46:43 +0600

    Added function and case total price increament according to case number

--- js/case.js
+++ js/case.js
@@ -1,8 +1,31 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease) {
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
 
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+
+    if (isIncrease) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
     caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 })
