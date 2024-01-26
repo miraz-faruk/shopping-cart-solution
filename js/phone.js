aa2e79730477ae86c567ea4fe78c61237f1a6ebd
mirazfaruk <mirazfaruk@gmail.com>
2024-01-26 22:36:41 +0600

    Add calculation for all products

--- js/phone.js
+++ js/phone.js
@@ -27,10 +27,12 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
     const newPhoneNumber = updatePhoneNumber(true);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
     const newPhoneNumber = updatePhoneNumber(false);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
